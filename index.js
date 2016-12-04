ad2278262ce4cf0af15e96ea6328b15eb39af95a
Keyvan <Keyvan@Keyvans-MacBook-Pro.local>
2016-12-04 16:26:50 +0900

    first app with polymr

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+//app.get('/', function(request, response) {
+//  response.render('pages/index');
+//});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
